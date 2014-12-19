552b928fc073a9496a865010aaadbf3c462d81bb
John Nissenbaum <niss615@aol.com>
2014-12-19 18:12:49 -0500

    week 3 program

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-
 #cachematrix takes as input a matrix (local variable x) and returns
 # a list with four items--each item is actually a function:
 #   get (returns the matrix)
